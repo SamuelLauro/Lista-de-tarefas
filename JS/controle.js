6e5b3bf568cb853ded36d76e65b5f4977ef8f5b9
SamuelLauro <samuellauro0612@gmail.com>
2023-06-19 19:20:39 -0300

    Criando a parte visual
    Parte visual pronta

---
+++ JS/controle.js
@@ -0,0 +1,12 @@
+let input = document.getElementById('tarefa');
+let btnAdd = document.getElementById('btn-add')
+
+function addTarefa(){
+    //Pegar o valor digitado no input
+    let valorInput = input.value;
+
+    //Se não for vazio, nem nulo, nem indefinido
+    if((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)){
+
+    }
+}
