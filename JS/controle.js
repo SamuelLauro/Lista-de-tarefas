523eb8f06e8e144b843d2b4633d8861810217cd2
SamuelLauro <samuellauro0612@gmail.com>
2023-06-19 22:23:11 -0300

    Update controle.js
    fiz algumas alterações, finalizando o projeto

--- JS/controle.js
+++ JS/controle.js
@@ -13,9 +13,9 @@ function addTarefa(){
         ++contador;
 
         let novoItem = ` <div id="${contador}" class="item">
-        <div onclick="marcarTarefa${contador})" class="item-icone">
-            <span id="icone_${contador}" class="material-symbols-outlined">
-                circle
+        <div onclick="marcarTarefa"${contador})" class="item-icone">
+            <span id="icone_${contador}" class="mdi-circle-outline">
+                
                 </span>
         </div>
         
@@ -47,24 +47,18 @@ function deletar(id){
 
 function marcarTarefa(id){
     var item = document.getElementById(id);
-    var classe = item.getAttribute('class')
+    var classe = item.getAttribute('class');
     console.log(classe);
 
-    if(classe=="item"){
+    if (classe =="item"){
         item.classList.add('clicado');
 
-        var icone = document.getElementById(`icone_${id}`);
-;
-        icone.classList.remove('material-symbols-outlined');
-        icone.classList.add('material-symbols-rounded');
+        var icone = document.getElementById('icone_'+id);
 
-
+        item.parentNode.appendChild(item);
+        
     } else{
         item.classList.remove('clicado');
-
-        var icone = document.getElementById('icone_${contador}');
-        icone.classList.remove('material-symbols-rounded');
-        icone.classList.add('material-symbols-outlined');
     }
 }
 
